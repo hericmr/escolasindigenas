9a1deb2e9335bfc7f71d7cd766f899e0d250d317
Héric Moura <heric.m.r@gmail.com>
2025-05-05 14:57:46 -0300

    Updates

--- src/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-// supabaseClient.js
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL; // ex.: "https://xyzcompany.supabase.co"
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY; // chave anônima do projeto
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
